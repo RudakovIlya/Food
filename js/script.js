82d87d5bd66a3bdc59d98d9c1a7230d22c399fc5
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 18:37:25 +0300

    feat: add comment

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
       2) Функция, которая будет показывать нужный нам таб.
 
-      3) Обработчик события на родителя табов.
+      3) Обработчик события на родителя табов. (Используем делегирование)
 
   */
 
@@ -24,23 +24,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabsContent.forEach(tabContent => {
 
-      tabContent.classList.add('hide')
+      tabContent.classList.add('hide')        // Перебираем контент табов и вешаем им класс для скрытия всех элементов.
 
-      tabContent.classList.remove('show', 'fade')
+      tabContent.classList.remove('show', 'fade') // Удаляем классы, которые показываю контет, а так же добавляют анимацию.
 
     });
 
-    tabs.forEach(tab => tab.classList.remove('tabheader__item_active'))
+    tabs.forEach(tab => tab.classList.remove('tabheader__item_active')); // Перебираем наши табы и удаляем класс активности, который выделяет текущий таб
 
   };
 
-  const showTabContent = (i = 0) => {
+  const showTabContent = (i = 0) => { // Устанавливаем значение по умолчанию, для того, что при перезагрузке страницы активным был всегда первый таб и его контент
 
-    tabsContent[i].classList.remove('hide')
+    tabsContent[i].classList.remove('hide') // Удаляем класс скрывающий контент конкретно у этого таба.
 
-    tabsContent[i].classList.add('show', 'fade')
+    tabsContent[i].classList.add('show', 'fade') // Добавляем классы. которые показывают текущий слайд, а так же добавляют анимацию появления
 
-    tabs[i].classList.add('tabheader__item_active');
+    tabs[i].classList.add('tabheader__item_active'); // Добавляем на текущий таб класс активности
 
   };
 
